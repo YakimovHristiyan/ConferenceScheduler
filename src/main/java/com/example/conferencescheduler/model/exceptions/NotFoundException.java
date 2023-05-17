73a5ac57c598a3af80b7895f765581081d3e08ab
Hristiyan Yakimov <yakimovhristiyan@gmail.com>
2023-05-17 16:11:09 +0300

    Configuration

--- src/main/java/com/example/conferencescheduler/model/exceptions/NotFoundException.java
+++ src/main/java/com/example/conferencescheduler/model/exceptions/NotFoundException.java
@@ -1,6 +1,6 @@
 package com.example.conferencescheduler.model.exceptions;
 
-public class NotFoundException extends Exception{
+public class NotFoundException extends RuntimeException {
     public NotFoundException(String msg) {
         super(msg);
     }
