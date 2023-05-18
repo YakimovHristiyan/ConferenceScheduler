b87b73eac980715c3c933ec2a647d33104ce8baa
Hristiyan Yakimov <yakimovhristiyan@gmail.com>
2023-05-18 11:54:20 +0300

    Added email verification feature

--- src/main/java/com/example/conferencescheduler/controllers/UserController.java
+++ src/main/java/com/example/conferencescheduler/controllers/UserController.java
@@ -3,12 +3,10 @@ package com.example.conferencescheduler.controllers;
 import com.example.conferencescheduler.model.dtos.userDTOs.UserRegisterDTO;
 import com.example.conferencescheduler.model.dtos.userDTOs.UserWithoutPassDTO;
 import com.example.conferencescheduler.model.services.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController extends AbstractController{
@@ -20,5 +18,11 @@ public class UserController extends AbstractController{
     @ResponseStatus(code = HttpStatus.CREATED)
     public UserWithoutPassDTO register(@RequestBody UserRegisterDTO dto){
         return userService.register(dto);
+    }
+
+    @GetMapping(value = "/users/email-verification")
+    public String verifyEmail(HttpSession session) {
+        int uid = getUserId(session);
+        return userService.verifyEmail(uid);
     }
 }
