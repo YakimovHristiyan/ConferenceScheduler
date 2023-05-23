edc447469228b9376ab9b6e9a0c357383b2fc04c
TeodorSimeonov <tp_si@abv.bg>
2023-05-23 14:03:31 +0300

    Added a check if the hour is used, sorted the timeslots

--- src/main/java/com/example/conferencescheduler/model/dtos/sessionDTOs/SessionDTO.java
+++ src/main/java/com/example/conferencescheduler/model/dtos/sessionDTOs/SessionDTO.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +17,7 @@ public class SessionDTO {
     private int conferenceId;
     private String name;
     private String description;
-    private List<LocalDateTime> bookedHours;
+    private List<LocalTime> bookedHours;
     private LocalDateTime endDate;
     private Integer hallId;
 }
--- src/main/java/com/example/conferencescheduler/model/services/SessionService.java
+++ src/main/java/com/example/conferencescheduler/model/services/SessionService.java
@@ -7,39 +7,76 @@ import com.example.conferencescheduler.model.entities.Conference;
 import com.example.conferencescheduler.model.entities.Hall;
 import com.example.conferencescheduler.model.entities.Session;
 import com.example.conferencescheduler.model.entities.User;
+import com.example.conferencescheduler.model.exceptions.BadRequestException;
 import com.example.conferencescheduler.model.exceptions.NotFoundException;
 import com.example.conferencescheduler.model.exceptions.UnauthorizedException;
+import com.google.api.client.util.DateTime;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class SessionService extends MasterService {
 
     @Transactional
-    public SessionDTO addSession(SessionDTO sessionDTO, int userId) {
-
+    public SessionDTO addSession(SessionDTO dto, int userId) {
         modelMapper.getConfiguration().setAmbiguityIgnored(true);
         User user = getUserById(userId);
-        Conference conference = getConferenceById(sessionDTO.getConferenceId());
-  
+        Conference conference = getConferenceById(dto.getConferenceId());
+        Hall hall = getHallById(dto.getHallId());
         validateOwnerRights(conference, user);
-        System.out.println(dto.getBookedHours());
+
+        List<LocalTime> sortedHours = sortBookedHours(dto.getBookedHours());
         Session session = Session.builder()
                 .name(dto.getName())
                 .conference(conference)
                 .description(dto.getDescription())
                 .hall(hall)
-                .startDate(dto.getBookedHours().get(0))
-                .endDate(dto.getBookedHours().get(dto.getBookedHours().size() - 1))
+                .startDate(LocalDateTime.of(conference.getStartDate().toLocalDate(), sortedHours.get(0)))
+                .endDate(LocalDateTime.of(conference.getStartDate().toLocalDate(), sortedHours.get(sortedHours.size() - 1)))
                 .build();
-        //Todo check if the hour is free to add this session
+
+        List<Session> bookedSessions = hall.getSessions();
+        iterateSessionAndCheckIfTheHoursFree(bookedSessions, session);
+
+        if(conference.getEndDate().isBefore(session.getEndDate())){
+        conference.setEndDate(session.getEndDate());
+        conferenceRepository.save(conference);
+        }
+
         sessionRepository.save(session);
-        //TODO if we want to set the end date time for the conference we need to get all the sessions and check which one is with the latest hour.
         hall.getSessions().add(session);
         hallRepository.save(hall);
         return dto;
+    }
+
+    private void iterateSessionAndCheckIfTheHoursFree(List<Session> bookedSessions, Session sessionWithWantedHours) {
+        LocalDateTime startTime = sessionWithWantedHours.getStartDate();
+        LocalDateTime endTime = sessionWithWantedHours.getEndDate();
+        boolean isHourFree = false;
+        for (Session bookedSession : bookedSessions) {
+            LocalDateTime startTimeOfExistingSession = bookedSession.getStartDate();
+            LocalDateTime endTimeOfExistingSession = bookedSession.getEndDate();
+            if ((startTime.isBefore(startTimeOfExistingSession) && endTime.isBefore(startTimeOfExistingSession))
+                    || (startTime.isAfter(endTimeOfExistingSession) && endTime.isAfter(endTimeOfExistingSession))) {
+                isHourFree = true;
+            }
+        }
+        if(!isHourFree){
+            throw new BadRequestException("Time frame is not free!");
+        }
+    }
+
+    private List<LocalTime> sortBookedHours(List<LocalTime> bookedHours) {
+        bookedHours.sort(Comparator.comparingInt(LocalTime::getHour));
+        return bookedHours;
     }
 
     public SessionDTO deleteSession(int userId, int sessionId) {
@@ -54,7 +91,7 @@ public class SessionService extends MasterService {
         return modelMapper.map(session, SessionDTO.class);
     }
 
-    public void validateOwnerRights(Conference conference, User user) {
+    private void validateOwnerRights(Conference conference, User user) {
         if (user.getUserRole().getRoleId() != CONFERENCE_OWNER_ROLE || conference.getOwner().getUserId() != user.getUserId()) {
             throw new UnauthorizedException("You are not owner of this conference!");
         }
